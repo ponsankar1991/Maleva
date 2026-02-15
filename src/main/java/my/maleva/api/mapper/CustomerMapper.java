25e5587441a56625bbe2caeefdae3f14d3eda6f4
karthickdeveloper0071 <karthick123svks@gmail.com>
2026-02-15 15:56:33 +0800

    add the customer mapper

---
+++ src/main/java/my/maleva/api/mapper/CustomerMapper.java
@@ -0,0 +1,23 @@
+package my.maleva.api.mapper;
+
+import my.maleva.api.dto.CustomerDto;
+import my.maleva.api.model.Customer;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring",
+        builder = @Builder(disableBuilder = true),
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface CustomerMapper {
+
+    CustomerDto toDto(Customer entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Customer toEntity(CustomerDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(CustomerDto dto, @MappingTarget Customer entity);
+}
