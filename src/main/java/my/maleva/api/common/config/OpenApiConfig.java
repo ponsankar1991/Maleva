ac9773bc5aa8c79d4fdf7334ad73f02870a48e98
karthickdeveloper0071 <karthick123svks@gmail.com>
2026-04-05 22:27:58 +0530

    add the api documentation

---
+++ src/main/java/my/maleva/api/common/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package my.maleva.api.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI malevaOpenApi() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Maleva API")
+                        .version("v1")
+                        .description("Interactive API documentation for the Maleva backend.")
+                        .contact(new Contact()
+                                .name("Maleva API Support"))
+                        .license(new License()
+                                .name("Internal Use")));
+    }
+}
