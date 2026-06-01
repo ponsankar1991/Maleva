ea86056955fc48be6228faa658df517d36fcc731
karthickdeveloper0071 <karthick123svks@gmail.com>
2026-06-01 10:03:51 +0530

    working on master classfication  file

---
+++ src/main/java/my/maleva/api/module/master/mapper/MasterClassificationMapper.java
@@ -0,0 +1,18 @@
+package my.maleva.api.module.master.mapper;
+
+import org.mapstruct.*;
+import my.maleva.api.module.master.entity.Classification;
+import my.maleva.api.module.master.dto.ClassificationDto;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface MasterClassificationMapper {
+
+    ClassificationDto toDto(Classification entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Classification toEntity(ClassificationDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(ClassificationDto dto, @MappingTarget Classification entity);
+}
